2ff92426347c83914fdba96f9c043f40cfffd4d0
ssangbomb <ssangbomb@gmail.com>
2023-06-10 02:59:49 -0700

    minor css topnavi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 import TopNavigation from "./components/TopNavigationBar";
-import LoginButton from "./Auth0/login";
-import LogoutButton from "./Auth0/logout";
 import Profile from "./Auth0/profile";
 
 const App = () => {
   return (
     <>
     <TopNavigation />
-    <LoginButton />
-    <LogoutButton />
     <Profile />
     </>
   )
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import '../styles/TopNavigationBar.scss'
+import LoginButton from "../Auth0/login";
+import LogoutButton from "../Auth0/logout";
 
 const TopNavigation = () => {
   return (
-    <div>
-      <span>Exam Genie</span>
+    <div className="top-nav-bar">
+      <span className="top-nav-bar--logo">Exam Genie</span>
+      <div>
+        <LoginButton/>
+        <LogoutButton/>
+      </div>
     </div>
   )
 }
