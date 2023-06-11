3bae5ad17ce7dfa513e5b04dcde2c6031b76455f
ssangbomb <ssangbomb@gmail.com>
2023-06-10 17:41:28 -0700

    merge together

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,66 +10,82 @@ function App() {
   const [answer, setAnswer] = useState("");
   const [feedback, setFeedback] = useState("");
 
-  const generateQuestion = async (questionValue) => {
-    const response = await fetch("http://localhost:8080/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ question: questionValue }),
-    });
-    const data = await response.json();
-    console.log(data.response); // Add this line
-    const questionData = data.response;
-    const { question, options, answer, feedback } =
-      extractQuestionData(questionData);
-      setQuestion(question);
-      setOptions(options);
-      setAnswer(answer);
-      setFeedback(feedback);
-      setSelectedOption(null); // Reset selected option
-
-    return { question, options, answer, feedback }; // Return the question data
-  };
-
   const onSubmit = async (e) => {
     e.preventDefault();
     const questionData = await generateQuestion(question);
     console.log("return from server", questionData);
   };
 
+  const generateQuestion = async (inputQuestion) => {
+    const response = await fetch("http://localhost:8080/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ question: inputQuestion }),
+    });
+    const data = await response.json();
+    console.log("data", data); // Log the response data
+
+    const { response: questionData } = data;
+    const { question, options, answer, feedback } =
+      extractQuestionData(questionData);
+
+    setQuestion(question);
+    setOptions(options);
+    setAnswer(answer);
+    setFeedback(feedback);
+    setSelectedOption(null);
+
+    return { question, options, answer, feedback };
+  };
+
   const extractQuestionData = (text) => {
     const lines = text.split("\n");
-  
+    console.log("lines", lines);
     let question = "";
     let options = [];
     let answer = "";
     let feedback = "";
-  
+
     const questionIndex = lines.findIndex((line) => line.startsWith("Q:"));
+    console.log("questionIndex", questionIndex);
     if (questionIndex !== -1) {
       question = lines[questionIndex].substring(3).trim();
     }
-  
-    const optionsStartIndex = lines.findIndex((line) => line.startsWith("A)"));
-    const answerIndex = lines.findIndex((line) => line.startsWith("Answer:"));
-    const feedbackIndex = lines.findIndex((line) => line.startsWith("Feedback:"));
-  
-    if (optionsStartIndex !== -1 && answerIndex !== -1 && feedbackIndex !== -1) {
-      options = lines.slice(optionsStartIndex, answerIndex).map((line) => {
-        const optionLetter = line.charAt(0);
-        const optionText = line.substring(3).trim();
-        return {
-          letter: optionLetter,
-          text: optionText,
-        };
-      });
-      answer = lines[answerIndex].substring(8).trim();
-      feedback = lines.slice(feedbackIndex + 1).join("\n").trim();
+
+    const optionsStartIndex = lines.findIndex((line) =>
+      line.trim().startsWith("A)")
+    );
+    console.log("optionsStartIndex", optionsStartIndex);
+    const answerIndex = lines.findIndex((line) =>
+      line.trim().startsWith("Answer:")
+    );
+    console.log("answerIndex", answerIndex);
+    const feedbackIndex = lines.findIndex((line) =>
+      line.trim().startsWith("Feedback:")
+    );
+    console.log("feedbackIndex", feedbackIndex);
+
+    if (
+      optionsStartIndex !== -1 &&
+      answerIndex !== -1 &&
+      feedbackIndex !== -1
+    ) {
+      options = lines
+      .slice(optionsStartIndex, answerIndex)
+      .map((line) => line.trim()) 
+      .filter((option) => option.length > 0); // Exclude empty options
+    answer = lines[answerIndex].substring(8).trim();
+    feedback = lines
+      .slice(feedbackIndex)
+      .filter((line) => line.trim().length > 0) // Exclude empty lines
+      .join("\n");
     }
-  
+
     return { question, options, answer, feedback };
   };
+  
   
   
   
@@ -80,49 +96,46 @@ function App() {
   };
 
   return (
-    <div>
-      <TopNavigation/>
-      <main className={style.main}>
-        <h3>EXAM-GENIE</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="query-description"
-            placeholder="question"
-            onChange={(e) => setQuestion(e.target.value)}
-          />
-          <input type="submit" value="generate question" />
-        </form>
+    <main className={style.main}>
+      <h3>EXAM-GENIE</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="question"
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <input type="submit" value="generate question" />
+      </form>
 
-        {question && (
-          <div>
-            <h2>{question}</h2>
-            <ul>
-              {options.map((option) => (
-                <li key={option.letter} className={style["radio-option"]}>
-                  <label>
-                    <input
-                      type="radio"
-                      name="options"
-                      value={option.letter}
-                      checked={selectedOption === option.letter}
-                      onChange={handleOptionChange}
-                    />
-                    {option.letter}) {option.text}
-                  </label>
-                </li>
-              ))}
-            </ul>
-            {selectedOption && (
-              <div>
-                <p>Answer: {answer}</p>
-                <p>Feedback: {feedback}</p>
-              </div>
-            )}
-          </div>
-        )}
-      </main>
-    </div>
+      {question && (
+        <div>
+          <h2>{question}</h2>
+          <ul>
+            {options.map((option) => (
+              <li key={option.letter} className={style["radio-option"]}>
+                <label>
+                  <input
+                    type="radio"
+                    name="options"
+                    value={option.letter}
+                    checked={selectedOption === option.letter}
+                    onChange={handleOptionChange}
+                  />
+                  {option.letter}) {option.text}
+                </label>
+              </li>
+            ))}
+          </ul>
+          {selectedOption && (
+            <div>
+              <p>Answer: {answer}</p>
+              <p>Feedback: {feedback}</p>
+            </div>
+          )}
+        </div>
+      )}
+    </main>
   );
 }
 
