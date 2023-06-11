fc2a7667f1ff81dba8e2ede5a965399fc7436688
Mathew Charm <mattcharmz@gmail.com>
2023-06-10 23:02:29 -0700

    Added react routes

--- frontend/src/components/TopMenu.jsx
+++ frontend/src/components/TopMenu.jsx
@@ -1,17 +1,28 @@
 import '../styles/TopMenu.scss';
 import Profile from '../Auth0/profile';
+import { Link } from "react-router-dom"
 
 const TopMenu = () => {
   return (
     <div className='top-nav-bar--menu-list'>
       <div className='topic-list--item'>
         <span>
-          <label>Results</label>
+          <Link to="/">Home</Link>
         </span>
       </div>
       <div className='topic-list--item'>
         <span>
-          <label>Profile</label>
+        <Link to="/exam">Test Exam</Link>
+        </span>
+      </div>
+      <div className='topic-list--item'>
+        <span>
+        <Link to="/about">About Us</Link>
+        </span>
+      </div>
+      <div className='topic-list--item'>
+        <span>
+        <Link to="/dashboard">Dashboard</Link>
         </span>
       </div>
     </div>
