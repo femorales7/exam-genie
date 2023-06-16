0745dfe144c675bc45e7658d4130edfd375550a5
Mathew Charm <mattcharmz@gmail.com>
2023-06-15 23:34:24 -0700

    Fixed TopMenu2

--- frontend/src/Auth0/profile.js
+++ frontend/src/Auth0/profile.js
@@ -12,8 +12,6 @@ const Profile = () => {
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
       </div>
     )
   );
--- frontend/src/components/TopMenu2.jsx
+++ frontend/src/components/TopMenu2.jsx
@@ -18,7 +18,7 @@ const TopMenu2 = () => {
   return (
     <div className="topNav">
       <div className="topNav-container">
-        <img src={logo} alt="brand" />
+        <img className="topNav-container--navigation--img" src={logo} alt="brand" />
         <div className="topNav-container--navigation">
           <Link to="/"><FaHome />Home</Link>
           <Link to="/about"><FaGithubAlt />About Us</Link>
@@ -29,9 +29,13 @@ const TopMenu2 = () => {
           </>
           )}
         </div>
-        <div>
-          {/* <Profile /> */}
-          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        <div className="topNav-profile">
+          <div className="topNav-profile--image">
+            <Profile className="topNav-profile--Icon"/>
+          </div>        
+          <div>
+            {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+          </div>
         </div>
       </div>
     </div>
