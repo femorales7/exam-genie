68da57b4b4b61f800a6cdb59623076dc867656a7
Your Name <your@email.com>
2023-06-21 17:35:56 -0600

    Css for create a oun exam and list og pervius exams

--- frontend/src/components/CreateExam.jsx
+++ frontend/src/components/CreateExam.jsx
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import "../styles/CreateExam.scss"
+import "../styles/CreateExam.scss";
 
 const CreateExam = () => {
   const navigate = useNavigate();
@@ -10,43 +10,57 @@ const CreateExam = () => {
   const examOnSubmit = (e) => {
     e.preventDefault();
 
-    const exam = { createExam }
-    fetch('http://localhost:8080/exam', {
-      method: 'POST',
-      headers: { "Content-Type" : "application/json"},
-      body: JSON.stringify(exam)
+    const exam = { createExam };
+    fetch("http://localhost:8080/exam", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(exam),
     }).then(() => {
-      console.log("exam created")
-    })
-  }
+      console.log("exam created");
+    });
+  };
 
   const onClickHandler = () => {
-    navigate("/exam")
-  }
+    navigate("/exam");
+  };
 
   return (
     <Container fluid className="project-section">
       <div className="exam-container">
-        <div className="exam-container-box">
+        <div className="">
           <div className="center">
             <form onSubmit={examOnSubmit}>
               <h2>Create your own exam</h2>
-              <input 
-              type="text"
-              placeholder="Create your exam"
-              value={createExam}
-              required
-              onChange={(e) => {
-                setCreateExam(e.target.value)
-              }}
-              />
-              <button type="submit" onClick={onClickHandler}>Create</button>
+              <div className="CreatExam">
+                <input
+                  // className="CreatExam"
+                  type="text"
+                  placeholder="Exam Name"
+                  value={createExam}
+                  required
+                  onChange={(e) => {
+                    setCreateExam(e.target.value);
+                  }}
+                />
+                {/* <label>Name of Exam</label> */}
+              </div>
+              <button
+                className="button-pushable"
+                type="submit"
+                value="Add user"
+                onClick={onClickHandler}
+              >
+                <span class="button-shadow"></span>
+                <span class="button-edge"></span>
+                <span class="button-front text">Create</span>
+              </button>
+              {/* <button type="submit" onClick={onClickHandler}>Create</button> */}
             </form>
           </div>
+        </div>
       </div>
-    </div>
-  </Container>
-  )
-}
+    </Container>
+  );
+};
 
 export default CreateExam;
